cbd485c3f9a14c8603a25ca567cf3e68b4858750
zw <zerowest1001@gmail.com>
2020-10-21 14:01:23 +0900

    Initial Commit

---
+++ aop/src/main/java/me/zw/aop/user/UserService.java
@@ -0,0 +1,5 @@
+package me.zw.aop.user;
+
+public interface UserService {
+    UserVO getUser(UserVO userVO);
+}
---
+++ aop/src/main/java/me/zw/aop/user/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package me.zw.aop.user.impl;
+
+import me.zw.aop.user.UserService;
+import me.zw.aop.user.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public UserVO getUser(UserVO userVO) {
+        return userDAO.getUser(userVO);
+    }
+}
